a928719a24708fc417f0babcbe96ad4bafe254bd
Juan Carlos Galvis Zuluaga <jcgalvis@bancolombia.com.co>
2022-05-15 11:28:30 -0500

    res1

--- src/main/java/co/com/bancolombia/challenges/TheJoyOfTheFirstOne.java
+++ src/main/java/co/com/bancolombia/challenges/TheJoyOfTheFirstOne.java
@@ -1,17 +1,37 @@
 package co.com.bancolombia.challenges;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Random;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 public class TheJoyOfTheFirstOne {
 
-    public int[] letsGoParty(int[] bigArray, int resultSize){
-        throw new UnsupportedOperationException();
-    }
+    public int[] letsGoParty(int[] bigArray, int resultSize) {
+        Map<Integer, Boolean> current = new TreeMap<>();
+        PriorityQueue<Integer> finalRes = new PriorityQueue<>();
+        PriorityQueue<Integer> comp = new PriorityQueue<>(1, Comparator.reverseOrder());
+        for (int val : bigArray) {
+            if (current.size() < resultSize) {
+                current.put(val, true);
+                comp.add(val);
+                finalRes.add(val);
+            } else if (comp.isEmpty() || comp.peek() > val) {
+                comp.add(val);
+                current.put(val, true);
+                finalRes.add(val);
+                current.remove(comp.poll());
+            }
+        }
 
+        int[] resa = finalRes.stream().mapToInt(x -> x).sorted().toArray();
+        int[] res = new int[resultSize];
+        System.arraycopy(resa, 0, res, 0, resultSize);
+        return res;
+    }
 
     public static void main(String[] args) throws IOException {
         TheJoyOfTheFirstOne joy = new TheJoyOfTheFirstOne();
@@ -22,8 +42,11 @@ public class TheJoyOfTheFirstOne {
 
         long init = System.currentTimeMillis();
         final int[] solution = joy.letsGoParty(array, 50);
-        System.out.println("Time: " + (System.currentTimeMillis()-init) + "ms");
-        System.out.println("Fin");
+        System.out.println("Time: " + (System.currentTimeMillis() - init) + "ms");
+        System.out.println("Fin" + solution[0]);
+        System.out.println("Fin" + solution[17]);
+        System.out.println("Fin" + solution[49]);
+        System.out.println("Fin" + solution[40]);
     }
 
 }
